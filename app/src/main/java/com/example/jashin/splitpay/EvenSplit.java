7023cfbb2bac94d7956380f8bc1a7edf7fb87d44
Dylan Pan <dpan117@gmail.com>
2018-07-19 19:01:04 -0400

    more if statements please

--- app/src/main/java/com/example/jashin/splitpay/EvenSplit.java
+++ app/src/main/java/com/example/jashin/splitpay/EvenSplit.java
@@ -11,6 +11,7 @@ public class EvenSplit extends AppCompatActivity {
     private float totalPrice;
     private int numberToSplit;
     private float finalSplitValue;
+    private String errorMsg = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,18 +21,40 @@ public class EvenSplit extends AppCompatActivity {
 
     public void splitEvenly() {
         EditText totalText = (EditText) findViewById(R.id.totalPrice);
-        totalPrice = Float.parseFloat(totalText.getText().toString());
         EditText numberSplitText = (EditText) findViewById(R.id.splitNumber);
-        numberToSplit = Integer.parseInt(numberSplitText.getText().toString());
+        String totalGetText = totalText.getText().toString();
+        String splitGetText = numberSplitText.getText().toString();
+        if (totalGetText.isEmpty() && splitGetText.isEmpty()) {
+            errorMsg = "You did not enter the total and the number of people.";
+        } else if (totalGetText.isEmpty()) {
+            errorMsg = "You did not enter the total.";
+        } else if (splitGetText.isEmpty()) {
+            errorMsg = "You did not enter the number of people.";
+        } else {
+            totalPrice = Float.parseFloat(totalText.getText().toString());
+            numberToSplit = Integer.parseInt(numberSplitText.getText().toString());
+            if ((totalPrice == (float) 0.0) && numberToSplit == 0) {
+                errorMsg = "You have entered 0.00 as the total and 0 as the number of people";
+            } else if (totalPrice == (float) 0.0) {
+                errorMsg = "You have entered 0.00 as the total.";
+            } else if (numberToSplit == 0) {
+                errorMsg = "You have entered 0 as the number of people.";
+            }
+        }
     }
 
     public void calculate(View view) {
         splitEvenly();
-        finalSplitValue = totalPrice / numberToSplit;
         System.out.println("totalPrice: " + totalPrice);
         System.out.println("numberToSplit: " + numberToSplit);
         System.out.println("finalSplitValue: " + finalSplitValue);
         TextView finalSplit = (TextView) findViewById(R.id.finalSplit);
-        finalSplit.setText(finalSplitValue + "");
+        if (errorMsg.isEmpty()) {
+            finalSplitValue = totalPrice / numberToSplit;
+            finalSplit.setText(finalSplitValue + "");
+        } else {
+            finalSplit.setText(errorMsg);
+            errorMsg = "";
+        }
     }
 }
